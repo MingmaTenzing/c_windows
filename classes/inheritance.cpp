c28eb9ef58cc3230525f9718197b48ec32dd9bef
Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-05-13 13:30:56 +0800

    multilevel inheritance

--- classes/inheritance.cpp
+++ classes/inheritance.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 class Vehicle
@@ -13,19 +13,36 @@ public:
     }
 };
 
-class Car : public Vehicle
+class Vehicle_Registration
+{
+
+public:
+    string rego_number;
+    int price;
+
+    Vehicle_Registration(string plate_number, int cost)
+    {
+        rego_number = plate_number,
+        price = cost;
+    }
+
+    void show_rego_details()
+    {
+        cout << "Registration Number is" << rego_number << "and " << "Cost is " << price;
+    }
+};
+
+class Car : public Vehicle, public Vehicle_Registration
 {
 public:
-    string model = " Toyota";
+    Car(string plate_num, int price) : Vehicle_Registration(plate_num, price) {}
 };
 
 int main()
 {
 
-    Car myCar;
-    cout << myCar.brand << endl;
-    cout << myCar.model << endl;
-    myCar.honk();
+    Car mycar("1idx", 333);
+    mycar.show_rego_details();
 
     return 0;
 }
