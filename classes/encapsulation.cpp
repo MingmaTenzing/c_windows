c23954152557ec7be9ae6000efc6a804c5cd276f
Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-05-13 12:52:44 +0800

    encapsulation

--- classes/encapsulation.cpp
+++ classes/encapsulation.cpp
@@ -10,16 +10,18 @@ private:
 
 public:
     string name;
-    int phone;
+    long long int phone;
     string location;
+
     Employee(string employee_name,
              int phone_number,
-             string address)
+             string address, int earning)
     {
 
         name = employee_name;
         phone = phone_number;
         location = address;
+        salary = earning;
     }
 
     void setSalary(int s)
@@ -27,12 +29,36 @@ public:
     {
         salary = s;
     }
+
     int getSalary()
     {
         return salary;
+    }
+
+    void show_user_details()
+    {
+        cout << name << endl;
+        cout << phone << endl;
+        cout << location << endl;
+        cout << salary << endl;
     }
 };
 
 int main()
 {
+    int employee_salary;
+    Employee first_employee("Mingma", 420639689, "Morely", 220);
+    Employee second_employee("Tenzing", 8893, "como", 10);
+
+    first_employee.show_user_details();
+    second_employee.show_user_details();
+
+    first_employee.setSalary(750);
+
+    employee_salary = first_employee.getSalary();
+    cout << employee_salary << endl;
+
+    first_employee.show_user_details();
+
+    return 0;
 }
